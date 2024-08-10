cfdd1f21a7ba788a6bde660ef9984d5ec902bac6
Emma Holden <emmaholden91@yahoo.com>
2024-08-10 23:01:51 +0100

    Added functionality to randomly place element when start button clicked

--- app.js
+++ app.js
@@ -1,3 +1,33 @@
+const animalWidth = 200;
+const animalHeight = 100
+const startButton = document.getElementById("start-button")
+const animalElement = document.getElementById('animal');
+const winWidth = window.innerWidth;
+const winHeight = window.innerHeight;
+const horizontalMax = (winWidth -animalWidth);
+const verticalMax = (winHeight - animalHeight);
+const yPos = Math.floor(getRandomNumber(0, verticalMax));
+const xPos = Math.floor(getRandomNumber(0, horizontalMax));
+let gameActive =  false;
+
+function getRandomNumber(min, max) {
+  return Math.random() * (max - min) + min;
+}
+
+startButton.addEventListener("click", () => {
+  animalElement.style.top = yPos +"px";
+  animalElement.style.left = xPos +"px";
+  gameActive = true;
+});
+
+animalElement.addEventListener("click", () => {
+  if (gameActive){
+    console.log("CLICKED ME")
+  }
+  
+})
+
+
 $(document).ready(function () {
   $(".lamp").on("animationiteration", function () {
     $(this).removeClass("swing");
